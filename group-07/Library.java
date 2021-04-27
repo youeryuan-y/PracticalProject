645767ad5748dbe44c72b1dfd9a25e3b0315a88b
yzw <yzw13185787572@163.com>
2021-04-28 01:39:30 +0800

    叶子文的小号提交交的代码

--- group-07/Library.java
+++ group-07/Library.java
@@ -1,7 +1,22 @@
-package git;
+﻿package git;
 
 public class Library {
     //TODO：叶子文
+String bookname;
+    String price;
+    String publish;
+
+    public String getBookname() {
+        return bookname;
+    }
+
+    public void setBookname(String bookname) {
+        this.bookname = bookname;
+    }
+
+    public String getPrice() {
+        return price;
+    }
 
     //TODO：陈思欣
 	public Library(String bookname, String price, String publish) {
