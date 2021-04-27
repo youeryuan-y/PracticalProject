4d69c05f76ea9f374b9f5ae9281064949d5a6a18
Isluo2000 <1610573302@qq.com>
2021-04-28 00:48:36 +0800

    罗宇欣git更新

--- group-07/git.java
+++ group-07/git.java
@@ -7,5 +7,12 @@ import java.util.List;
 
 public class git {
     //TODO：罗宇欣
+    public static void main(String[] args) {
+        Connection connection= DBconnection.getConnection();
+        LibraryDao libraryDao=new LibraryDao();
+        libraryDao.query("C Primer Plus 第6版 中文版(异步图书出品)");
+//        libraryDao.deleteByBookname("rjgc");
+//        libraryDao.insert("rjgc");
+    }
 
 }
