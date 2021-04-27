5ec2a4a9562a7e5b2027a4d516bb6e6c9579d15f
Isluo2000 <1610573302@qq.com>
2021-04-28 00:49:33 +0800

    罗宇欣根据书名查询更新1

--- group-07/LibraryDao.java
+++ group-07/LibraryDao.java
@@ -13,7 +13,20 @@ import java.util.List;
 public class LibraryDao {
 //    TODO：罗宇欣
 //    根据书名名查询
-
+     public void query(String bookname){
+        //    根据书名名查询
+        List<Library> libraries=new ArrayList<>();
+        Connection connection= DBconnection.getConnection();// 获取连接
+        Statement statement=null;
+        ResultSet set=null;
+        try {
+            statement =connection.createStatement(); //通过连接获取 statement 对象
+            String selectStr="";
+            if (bookname!=null&&!bookname.equals("")){
+                selectStr="select * from library where bookname like '%"+bookname+"%'";
+            }else {
+                selectStr="select * from library ";
+            }
     //删除
     //TODO：周帮华
     public void deleteByBookname(String bookname){
