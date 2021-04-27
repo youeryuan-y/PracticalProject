f36f6f765e90ce1377a52c7a47fb9e9c7a9c0cf0
zbh12 <2941194765@qq.com>
2021-04-27 23:55:35 +0800

    删除

--- group-07/LibraryDao.java
+++ group-07/LibraryDao.java
@@ -16,6 +16,36 @@ public class LibraryDao {
 
     //删除
     //TODO：周帮华
+    public void deleteByBookname(String bookname){
+        Connection connection=DBconnection.getConnection();// 获取连接
+        Statement statement=null;
+        try {
+            statement =connection.createStatement(); //通过连接获取 statement 对象
+            String deleteStr="delete from library where bookname="+'"'+bookname+'"';
+            int i= statement.executeUpdate(deleteStr);  //通过 statement 执行sql 语句  返回操作的条数
+            if (i>=0){
+                System.out.println( "删除成功");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if (statement!=null){
+                try {
+                    statement.close();  //关闭连接
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection!=null){
+                try {
+                    connection.close(); //关闭连接
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 
     //添加
     //TODO：周帮华
